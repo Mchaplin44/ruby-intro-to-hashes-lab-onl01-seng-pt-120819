3881f63eef23da4c468cacf4c4fae46879260643
Martin Chaplin <martin.chaplin44@gmail.com>
2019-12-22 20:36:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,12 +1,71 @@
-ikea = 
-new_hash = []
+ikea = {:chair => 25, :table => 85, :mattress => 450}
+
+# This didn't pass because it calls the "value" method
+def key_for_min_value_my_try(name_hash)
+  name_hash.key(name_hash.values.min)
+end
+
+key_for_min_value_my_try(ikea)
+
+# This is learn.co's passing solution
+
+def key_for_min_value(hash)
+  smallest_key = nil
+  tiniest_value = nil
+  hash.each do |key, value|
+    if tiniest_value == nil || value < tiniest_value
+      tiniest_value = value
+      smallest_key = key
+    end
+  end
+  smallest_key
+end
+
+key_for_min_value(ikea)
+
+
+  epic_tragedy = {
+   :montague => {
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => [
+         {name: "Benvolio", age: "17", attitude: "worried"},
+         {name: "Steven", age: "30", attitude: "confused"}
+      ]
+   }, 
+   :capulet => {
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => [
+          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
+          {name: "Nurse", age: "44", attitude: "worried"}
+      ]
+   }
+  }
+  
+epic_tragedy[:montague][:hero][:status] = "dead"
+epic_tragedy
+  
+  
 def monopoly
+  monopoly = {}
+  monopoly[:railroads] = {}
 end 
   
 monopoly
 
 
-# 
+# The following method won't work... the correct way is below:
+
+# def monopoly_with_third_tier
+#  monopoly[:pieces] = 4,
+#  monopoly[:rent_in_dollars] = "one_piece_owned", "two_pieces_owned", "three_pieces_owned", "four_pieces_owned"
+#  monopoly[:names] = "reading_railroad", "pennsylvania_railroad", "b_and_o_railroad", "shortline"
+# end 
+
+
 # This one did not pass the learn.co test because of the following error message:
 
 # expected: [:pieces, :names, :rent_in_dollars]
@@ -121,3 +180,8 @@ def monopoly_with_fourth_tier
     }
   }
 end
+
+monopoly_with_fourth_tier
+
+
+
